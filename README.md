e186e2305654010f47be162d79ec3ecf3e6e2bca
Raffenio <mintaka.star@gmail.com>
2025-01-30 22:45:02 -0700

    clean

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # code-challenges
 here I will add code challenges that I'm doing from different pages.
+
+
+First challenge is from [NeetCode: top k elements in list](https://neetcode.io/problems/top-k-elements-in-list)
+
--- src/com/raffenio/roadmap/arrays/and/hashing/top/k/elements/in/list/Main_TopKElementsInList.java
+++
@@ -1,7 +0,0 @@
-package com.raffenio.roadmap.arrays.and.hashing.top.k.elements.in.list;
-
-public class Main_TopKElementsInList {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
