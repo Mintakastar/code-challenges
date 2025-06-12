e64d860d109613a05369d524bf1c2f896e0e92b4
raffenio <mintaka.star@gmail.com>
2025-06-11 23:50:11 -0700

    Add the lonely example, in this case using first and last index, to validate duplicity

--- src/com/raffenio/hackerrank/com/challenges/one/week/preparation/kit/lonely/integer/LonelyInteger.java
+++ src/com/raffenio/hackerrank/com/challenges/one/week/preparation/kit/lonely/integer/LonelyInteger.java
@@ -13,17 +13,32 @@ public class LonelyInteger {
 
         // Example 1
         List<Integer> example1 = Arrays.asList(1,2,3,4,3,2,1);
-        int lonelyInteger = solution.lonelyinteger(example1);
+        int lonelyInteger = solution.lonelyintegerSorting(example1);
+        System.out.println("Input: [1, 2, 3, 4, 3, 2, 1]");
+        System.out.println("Expected output: 4");
+        System.out.println("Algorithm Output: " + lonelyInteger);
+        System.out.println("");
+
+        example1 = Arrays.asList(1,2,3,4,3,2,1);
+        lonelyInteger = solution.lonelyintegerIndex(example1);
         System.out.println("Input: [1, 2, 3, 4, 3, 2, 1]");
         System.out.println("Expected output: 4");
         System.out.println("Algorithm Output: " + lonelyInteger);
 
 
         System.out.println("");
+        System.out.println("");
 
         // Example 2
         List<Integer> example2= Arrays.asList(1,1,2);
-        lonelyInteger = solution.lonelyinteger(example2);
+        lonelyInteger = solution.lonelyintegerSorting(example2);
+        System.out.println("Input: [1, 1, 2]");
+        System.out.println("Expected output: 2");
+        System.out.println("Algorithm Output: " + lonelyInteger);
+        System.out.println("");
+
+        example2= Arrays.asList(1,1,2);
+        lonelyInteger = solution.lonelyintegerIndex(example2);
         System.out.println("Input: [1, 1, 2]");
         System.out.println("Expected output: 2");
         System.out.println("Algorithm Output: " + lonelyInteger);
--- src/com/raffenio/hackerrank/com/challenges/one/week/preparation/kit/lonely/integer/Solution.java
+++ src/com/raffenio/hackerrank/com/challenges/one/week/preparation/kit/lonely/integer/Solution.java
@@ -5,7 +5,7 @@ import com.raffenio.base.SolutionBase;
 
 import java.util.List;
 
-public class Solution extends SolutionBase {
+public class Solution {
 
     /*
      * Complete the 'lonelyinteger' function below.
@@ -13,23 +13,38 @@ public class Solution extends SolutionBase {
      * The function is expected to return an INTEGER.
      * The function accepts INTEGER_ARRAY a as parameter.
      */
-
-    public static int lonelyinteger(List<Integer> a) {
+    public static int lonelyintegerSorting(List<Integer> a) {
+        System.out.println("Sorting Algorithm");
         a.sort(Integer::compareTo);
-
         for (int i = 0; i < a.size(); i++) {
-
             if(i==a.size()-1){
                 return a.get(i);
             }
-
             if( a.get(i) != a.get(i+1)){
                 return a.get(i);
             }else{
                 i++; // skip the next one since it's the same
             }
         }
+        return -1;
+    }
 
+
+    /*
+     * Complete the 'lonelyinteger' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+    public static int lonelyintegerIndex(List<Integer> a) {
+        System.out.println("Index Algorithm");
+
+        for (int i = 0; i < a.size(); i++) {
+            //if the last and first index are the same, it means there is only one occurrence
+           if(a.indexOf(a.get(i)) == a.lastIndexOf(a.get(i))){
+               return a.get(i);
+           }
+        }
         return -1;
     }
 }
